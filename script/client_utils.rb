20860f97521bb9f97cf456eb7512f74fba93756d
Rebecca Goodwin <rebeccag@spiceworks.com>
2018-02-26 10:20:00 -0600

    Move some things around, fix bug

---
+++ script/client_utils.rb
@@ -0,0 +1,28 @@
+require 'yaml'
+require 'fitgem_oauth2/client.rb'
+require 'rails' # TODO
+
+# https://dev.fitbit.com/build/reference/web-api/oauth2/
+# https://dev.fitbit.com/apps/oauthinteractivetutorial
+
+
+@root_filepath = "/Users/rebeccag/repositories/fitbit_stats"
+
+def get_client
+  secrets = YAML.load(File.read("secrets.yml"))
+  client = FitgemOauth2::Client.new(
+    client_id: secrets[:client_id],
+    client_secret: secrets[:client_secret],
+    token: secrets[:access_token])
+end
+
+def refresh_token!(client)
+  secrets = YAML.load(File.read("secrets.yml"))
+  response = client.refresh_access_token(secrets[:refresh_token])
+  new_token = response["access_token"]
+  secrets[:access_token] = new_token
+  File.write("secrets.yml", YAML.dump(secrets))
+
+  client = get_client
+end
+
--- script/get_hr.rb
+++ script/get_hr.rb
@@ -1,43 +1,21 @@
 require 'yaml'
 require 'fitgem_oauth2/client.rb'
 require 'rails' # TODO
-# https://dev.fitbit.com/build/reference/web-api/oauth2/
-# https://dev.fitbit.com/apps/oauthinteractivetutorial
 
-@filepath = "../data/heart_rate_json/"
+require_relative 'client_utils'
 
-
-def get_client
-  secrets = YAML.load(File.read("secrets.yml"))
-  client = FitgemOauth2::Client.new(
-    client_id: secrets[:client_id],
-    client_secret: secrets[:client_secret],
-    token: secrets[:access_token])
-end
-
-def refresh_token!(client)
-  new_token = client.refresh_access_token(secrets[:refresh_token])["access_token"]
-  secrets[:access_token] = new_token
-  File.write("secrets.yml", YAML.dump(secrets))
-
-  client = get_client
-end
-
+@filepath = "#{@root_filepath}/data/heart_rate_json/"
 
 client = get_client
-# response = client.heart_rate_time_series(start_date: 1.month.ago, end_date: Date.yesterday)
-# hrs = response["activities-heart"]
+# refresh_token!(client) # use if my token has expired
 
-# # intraday_heart_rate_time_series(start_date: nil, end_date: nil, detail_level: nil, start_time: nil, end_time: nil)
-# client.intraday_heart_rate_time_series(start_date: Date.parse("2018-02-07"), end_date: Date.parse("2018-02-08"), detail_level: "1min", start_time: DateTime.parse("2018-02-07 00:00:00"), end_time: DateTime.parse("2018-02-08 00:00:00"))
-
-hrs = client.intraday_heart_rate_time_series(start_date: Date.parse("2018-02-07"), end_date: Date.parse("2018-02-07"), detail_level: "1min", start_time: DateTime.parse("2018-02-07 00:00:00"), end_time: DateTime.parse("2018-02-07 23:59:59"))
+hrs = client.intraday_heartrate_time_series(start_date: Date.parse("2018-02-07"), end_date: Date.parse("2018-02-07"), detail_level: "1min", start_time: DateTime.parse("2018-02-07 00:00:00"), end_time: DateTime.parse("2018-02-07 23:59:59"))
 hrs["activities-heart-intraday"]["dataset"].map {|hr| [hr["time"], hr["value"]]}
 
 def write_file_for_date(date)
   # date = DateTime.parse(datestring)
   puts "Getting data for #{date}"
-  hrs = @client.intraday_heart_rate_time_series(
+  hrs = @client.intraday_heartrate_time_series(
     start_date: date,
     end_date: date,
     detail_level: "1min",
@@ -57,8 +35,6 @@ def convert_to_csv(json)
 end
 
 @client = get_client
-# JSON.parse(File.read("data/180207.json"))
-# write_file_for_date("2018-02-6")
 
 def get_data!(force=false)
   date_range = if force
